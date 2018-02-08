176d899c4a40a06486f85dbee2861c2f47a11d61
2015-08-26 <akim@janus14.cs.trinity.edu>
2018-02-08 11:47:05 -0600

    Setting up Git repository to school computer

---
+++ WebApps/javascript.js
@@ -0,0 +1,23 @@
+console.log("We got here!");
+document.getElementsByTagName("p")[0]
+
+function incTime(delta) {
+    let elem = document.getElementById("time");
+    elem.innerHTML = parseInt(elem.innerText)+delta;
+}
+
+setInterval(function() {incTime(5);}, 1000);
+
+function printMessage() {
+    console.log("Thank you for clicking.");
+}
+
+let canvas = document.getElementById("canvas");
+let gc = canvas.getContext('2d');
+console.log(gc.fillStyle);
+let grad = gc.createLinearGradient(0,0,150,100);
+grad.addColorStop(0.0, "red");
+grad.addColorStop(1.0, "green");
+gc.fillStyle = grad;
+gc.fillRect(100, 50, 200, 150);
+
